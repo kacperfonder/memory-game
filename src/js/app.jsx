90d450525ded45832f742b2a1f55bec0af243cd7
kacperfonder <kacper.fonder@gmail.com>
2018-02-23 14:52:44 +0100

    memory game

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -14,31 +14,33 @@ class GameBoard extends React.Component {
             cardOneFlipped: false,
             cardTwoFlipped: false,
             movesDone: 0,
-            display: 'none',
+            class: ""
            
         }
     }
     matched = () => {
-    
+      
         this.state.checkedCards = [];
         
     }
     unmatched = () => {
-        this.setState({
-            
-        })
+     
         this.state.checkedCards = [];
 
     }
+   
+
+
 
     handleClick = (e) => {
- 
+        this.setState({
+            class: this.state.class == 'rotate' ? '' : 'rotate'
+        })
+
         this.state.checkedCards.push(e.currentTarget);
-        console.log("click");
+        
         console.log(this.state.checkedCards);
-        if (e.currentTarget === 'none') {
-            this.setState({ display: 'block' })
-        }
+        
         if(this.state.checkedCards.length == 2){  
             
             if( this.state.checkedCards[0].firstElementChild.alt === this.state.checkedCards[1].firstElementChild.alt){
@@ -52,13 +54,14 @@ class GameBoard extends React.Component {
                 this.unmatched();
                
             }
+
         }
     };
-  
+   
+    
     render() {
-      
     this.state.getCards = cards.map((e,i) =>{
-        return  <div onClick={this.handleClick} key={i}>  <img  src={e.src} alt={e.name} id={e.id}/>   </div>
+        return  <div  onClick={this.handleClick} key={i}>  <img className={this.state.class}  src={e.src} alt={e.name} id={e.id}/>   </div>
     })
 
     function shuffle (array) {
@@ -72,11 +75,12 @@ class GameBoard extends React.Component {
         }
         return array;
     }
+   
+
 
         return (
             <section className='board'> 
-               {shuffle(this.state.getCards)}
-
+                {shuffle(this.state.getCards)}
             </section>
         );
     }
