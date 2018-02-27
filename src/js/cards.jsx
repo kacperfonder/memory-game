93b601f3b8267a30af38d7f17b988c6feb1378d8
kacperfonder <kacper.fonder@gmail.com>
2018-02-27 15:32:33 +0100

    memory game

---
+++ src/js/cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    ReactDOM.render (
+        <h1>Tytul</h1>,
+        document.querySelector('#app')
+    )
+});
